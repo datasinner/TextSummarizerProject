8411a7237577bb42ab02bc3de022be1bfaa1f3e3
datasinner <dilmurodmadrakhimov@gmail.com>
2023-06-14 10:47:16 +0500

    adding os to model_trainer.py

--- src/textSummarizer/components/model_trainer.py
+++ src/textSummarizer/components/model_trainer.py
@@ -2,8 +2,9 @@ from transformers import TrainingArguments, Trainer
 from transformers import DataCollatorForSeq2Seq
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 from datasets import load_dataset, load_from_disk
-import torch
 from textSummarizer.entity import ModelTrainerConfig
+import torch
+import os
 
 class ModelTrainer:
     def __init__(self, config: ModelTrainerConfig):
